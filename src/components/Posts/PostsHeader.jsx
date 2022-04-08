5ab9969a10a29098a106c1028f1b105137ae4e4f
Timofey Chunarev <timofei.chunarev@gmail.com>
2022-04-08 15:41:59 +0700

    add some sort logic

---
+++ src/components/Posts/PostsHeader.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const PostsHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+` 
+
+export default PostsHeader;
--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
 import { IPost } from '../../../interfaces/interfaces';
 import Post from './Post';
+import PostsHeader from './PostsHeader';
+import { useState, useEffect } from 'react';
 
 const Posts = styled.div`
     width: 60%;
     margin: 0 auto;
     padding: 15px;
-
-    input {
-        margin-bottom: 20px;
-    }
 
     .post {
         margin-bottom: 20px;
@@ -22,10 +20,50 @@ interface PostsProps {
 }
 
 const Index = ({className, data}: PostsProps) => {
+    
+    const [sortStatus, setSortStatus] = useState('');
+    const [booleanSortFlag, setBooleanSortFlag] = useState(false);
+
+    useEffect(() => {
+        if (booleanSortFlag) {
+            setSortStatus('ASC');
+            sortHandler(sortStatus, data);
+        } else {
+            setSortStatus('DESC')
+            sortHandler(sortStatus, data);
+        }
+    })
+
+    function sortHandler(sortStatus: string, data: IPost[]) {
+        switch(sortStatus) {
+            case "ASC": 
+                data.sort((a, b) => {
+                    if (a.id > b.id) {
+                        return 1
+                    } else {
+                        return -1
+                    }
+                });
+                break;
+            case "DESC": 
+                data.sort((a, b) => {
+                    if (a.id > b.id) {
+                        return -1
+                    } else {
+                        return 1
+                    }
+                });
+                break;
+        }
+    }
 
     return(
         <Posts className={className}>
-            <input type="text" placeholder='Поиск'/>
+            <PostsHeader>
+                <input type="text" placeholder='Поиск'/>
+                <button onClick={() => setBooleanSortFlag(!booleanSortFlag)}>{sortStatus}</button>
+            </PostsHeader>
+
             {data.map((item, index) => {
                 return(
                     <Post className='post' post={item} key={index}/> 
