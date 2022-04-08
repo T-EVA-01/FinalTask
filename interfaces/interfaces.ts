3acaa09797b85a92ca30d8283b66321f954a09ae
Timofey Chunarev <timofei.chunarev@gmail.com>
2022-04-08 13:01:48 +0700

    create interfaces.ts

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,18 @@
+export interface IOptions {
+    sort: 'ASC' | 'DESK'
+    filter: {
+        name: string
+        value: string
+    }
+}
+
+export interface IPost {
+    id: number
+    title: string
+    description: string
+    image: string
+} 
+
+export interface IndexPageProps {
+    posts: IPost[]
+}
